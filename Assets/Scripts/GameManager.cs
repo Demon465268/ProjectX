737243227f7ce373c8da0ce0a51fd95d091c0730
GrizzledBoor <danyzaitsev@gmail.com>
2016-10-02 17:09:34 +0300

    Скрипт генерации уровня (только вперед)
    + префабы для генерации
    + простенький код, позволяющий игроку постоянно двигаться вперед

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using UnityEngine;
+using System.Collections;
+using UnityEngine.Rendering;
+using Random = UnityEngine.Random;
+
+public class GameManager : MonoBehaviour
+{
+
+    public GameObject[] tilePrefabs;
+    private Transform playerTransform;
+    private float spawnZ = 0.0f;
+    private float tileLength = 5f;
+    private int amountOfTileOsScreen = 5;
+    public float spX = 0;
+    public float spY = 6;
+    public float spZ = 1;
+    // Use this for initialization
+	void Start ()
+	{
+	    playerTransform = GameObject.FindGameObjectWithTag("Player").transform;
+	    for (int i = 0; i < amountOfTileOsScreen; i++)
+	    {
+	        SpawnTile();
+	    }
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	    if (playerTransform.position.z > (spawnZ - amountOfTileOsScreen * tileLength))
+	    {
+	        SpawnTile();
+	    }
+	}
+
+    private void SpawnTile(int PrefabIndex = -1)
+    {
+        
+        GameObject go;
+        go = Instantiate(tilePrefabs[0/*Convert.ToInt32(Random.Range(0,2))*/]) as GameObject;
+        go.transform.SetParent(transform);
+        go.transform.position = new Vector3(spX, spY, spZ * spawnZ);
+        spawnZ += tileLength;
+    }
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEditor;
+
+public class PlayerController : MonoBehaviour
+{
+
+    public float speed = 10f;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+        GetComponent<Rigidbody>().velocity = new Vector3(0.0f, 0.0f, speed);
+    }
+}
