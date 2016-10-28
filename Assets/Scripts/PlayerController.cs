0f393d26379e9141a8c4f15e69b01cde9788cd93
GrizzledBoor <danyzaitsev@gmail.com>
2016-10-28 19:28:48 +0300

    Enos - IsKinematic. Бег вперед и повороты работают

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,64 +4,46 @@ using UnityEditor;
 
 public class PlayerController : MonoBehaviour
 {
-    private CharacterController controller;
-    public Vector3 moveVector;
-    public float maxSpeed;
-    public float verticalVelocity;
-    private const float gravity = 12.0f;
-    public float speed = 0.01f;
-
-    public float smooth = 1f;
     private Quaternion targetRotation;
-
+    private Vector3 targetPosition;
+    public float moveSpeed;
+    public float verticalVelocity;
+    public float rotationSmooth = 1.0f;
 
     void Start()
     {
-        GetComponent<Rigidbody>().transform.Translate(Vector3.forward * speed, Space.Self);
-        controller = GetComponent<CharacterController>();
         targetRotation = GetComponent<Rigidbody>().transform.rotation;
+        targetPosition = GetComponent<Rigidbody>().transform.position;
     }
 
 
     void Update()
     {
-        GetComponent<Rigidbody>().transform.Translate(new Vector3(0.0f, 0.0f, 0.1f)/*Vector3.forward*speed*/, Space.Self);
-        if (Input.GetKeyDown(KeyCode.RightArrow))
+
+        if (Input.GetKeyDown(KeyCode.A))
         {
-            //GetComponent<Rigidbody>().transform.Rotate(Vector3.up*90, Space.Self);
-            //GetComponent<Rigidbody>().transform.Translate(new Vector3(0.1f, 0.0f, 0.0f), Space.Self);
+            targetRotation *= Quaternion.AngleAxis(-90, Vector3.up);
+        }
+
+        if (Input.GetKeyDown(KeyCode.D))
+        {
             targetRotation *= Quaternion.AngleAxis(90, Vector3.up);
         }
-        
-        if (Input.GetKeyDown(KeyCode.LeftArrow))
+
+        if (Input.GetKey(KeyCode.Q))
         {
-            //GetComponent<Rigidbody>().transform.Rotate(Vector3.up * -90, Space.Self);
-            //GetComponent<Rigidbody>().transform.Translate(new Vector3(-0.1f, 0.0f, 0.0f), Space.Self);
-            targetRotation *= Quaternion.AngleAxis(-90, Vector3.up);
-
+            targetPosition = Vector3.forward + Vector3.left;
         }
-        transform.rotation = Quaternion.Lerp(transform.rotation, targetRotation, 10 * smooth * Time.deltaTime);
+        else if (Input.GetKey(KeyCode.E))
+        {
+            targetPosition = Vector3.forward + Vector3.right;
+        }
+        else
+        {
+            targetPosition = Vector3.forward;
+        }
 
-
-
-        moveVector = Vector3.zero;
-           if (controller.isGrounded)
-           {
-               verticalVelocity = -0.5f;
-           }
-           else
-           {
-               verticalVelocity -= gravity * Time.deltaTime;
-           }
-        //X - Left and Right
-        moveVector.x = Input.GetAxisRaw("Submit") * maxSpeed;
-        //Y - Up and Down
-        moveVector.y = verticalVelocity;
-        //Z - Forward and Backward
-        // moveVector.z = maxSpeed;
-
-        controller.Move(moveVector * Time.deltaTime);
+        transform.Translate(targetPosition * moveSpeed * Time.deltaTime, Space.Self);
+        transform.rotation = Quaternion.Lerp(transform.rotation, targetRotation, 10 * rotationSmooth * Time.deltaTime);
     }
 }
- 
-
